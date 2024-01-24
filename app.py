68fb73385702e0bc2e6672fa7bf47c3a0805f379
Daniel-Avo <daniel.avoyan21@gmail.com>
2024-01-24 11:59:20 +0100

    flask update

--- app.py
+++ app.py
@@ -4,3 +4,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello world'
+
+@app.route('/env')
+def env():
+    return 'DISCLOSED_STRING'
+
+@app.route('/secret')
+def secret():
+    return 'SECRET_STRING'
